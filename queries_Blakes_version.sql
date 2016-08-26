db2cd504a1dbfceb0dbe7fe58ce08ab94bc7d424
Blake <BlakeJohnCarter@users.noreply.github.com>
2016-08-26 15:43:10 +1200

    Add files via upload

---
+++ queries_Blakes_version.sql
@@ -0,0 +1,53 @@
+--1: a SELECT using a LIKE something
+--Finding a staff members address starting with "I"
+SELECT * 
+FROM staff
+WHERE s_address LIKE 'I%';
+
+
+--2: a SELECT on a DATE column
+--Finding a staff members DOB
+SELECT * 
+FROM staff
+WHERE date_of_birth = TO_DATE('12-12-1996','DD-MM-YYYY');
+
+--3: a query using GROUP BY and HAVING
+--Finding staff memebers with a salary larger than 100,000
+SELECT s_fname, MAX(salary) 
+FROM staff
+GROUP BY s_fname
+HAVING MAX(salary) > 100000;
+
+--4: a query using a subquery
+--Finding genders of customers from account
+
+SELECT *
+       FROM account
+       WHERE ca_id IN
+             (SELECT c_id
+                     FROM customer
+                     WHERE c_gender = 'F');
+
+--5: a query using a correlated subquery
+-- finds all staff that have a higher salary than the average salary
+
+
+
+
+
+
+--6: a query involving COUNT(*)
+-- finds customers whose balance is over the average customer balance
+
+SELECT *
+       FROM customer
+       WHERE c_ID IN
+             (SELECT c_ID
+                     FROM account
+                     WHERE balance > AVG(balance));
+
+
+--7: 
+ 
+
+ 
